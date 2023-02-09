661f7c92164da693705eb51c8fff584c07e1a850
Tacherifte Aymen <ch.tacherifte@gmail.com>
2023-02-09 22:15:01 +0100

    update routing module

--- Authentification/src/app/app-routing.module.ts
+++ Authentification/src/app/app-routing.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
 import { AdminComponent } from './admin/admin.component';
-
 import { AuthentificationComponent } from './authentification/authentification.component';
+import { AuthentificationGuard } from './authentification.guard';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'login'},
   {path: 'auth', component: AuthentificationComponent},
-  {path: 'auth', component: AdminComponent}
+  {path: 'auth', component: AdminComponent, canActivate:[AuthentificationGuard]}
 ];
 
 @NgModule({
